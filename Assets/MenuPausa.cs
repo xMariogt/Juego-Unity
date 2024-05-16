60dff67d640ff90d5e844473c86e5e0e4d91f028
JeffersonArriaza <jefri.arriaza@gmail.com>
2024-05-16 17:02:33 -0600

    Agrega opcion de botones al menu pausa, y agrega escena ganadora

--- Assets/MenuPausa.cs
+++ Assets/MenuPausa.cs
@@ -9,14 +9,30 @@ public class MenuPausa : MonoBehaviour
     [SerializeField] private GameObject MenuPause;
     [SerializeField] private GameObject btnPausa;
 
+    private bool pausaJuego = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pausaJuego)
+            {
+                resume();
+            }else {
+                pausa();            
+            }
+        }
+    }
+
     public void pausa()
     {
+        pausaJuego = true;
         Time.timeScale = 0f;
         btnPausa.SetActive(false);
         MenuPause.SetActive(true);
     }
     public void resume ()
     {
+        pausaJuego=false;
         Time.timeScale = 1f;
         btnPausa.SetActive(true);
         MenuPause.SetActive(false);
@@ -24,6 +40,7 @@ public class MenuPausa : MonoBehaviour
 
     public void Iniciar()
     {
+        pausaJuego = false;
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
     }
--- Assets/Scripts/MetaFinal.cs
+++ Assets/Scripts/MetaFinal.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MetaFinal : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")){
-            Debug.Log("Ganaste");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
         }
     }
 }
